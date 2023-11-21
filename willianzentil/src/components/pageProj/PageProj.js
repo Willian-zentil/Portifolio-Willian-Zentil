cb4a9b333b2d051126091a46aaf3ff833dee5512
willianZentil <willian.zentil@maeztra.com>
2023-11-21 07:16:00 -0300

    feature#projetos

--- willianzentil/src/components/pageProj/PageProj.js
+++ willianzentil/src/components/pageProj/PageProj.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import ContainerSmall from '../containerSmall/ContainerSmall'
 import Header from '../header/Header'
 import Background from '../atoms/backGround/Background'
 import styles from './PageProj.module.css'
 
-import seralle from '../../images/seralle.png'
+import seralle from '../../images/seralleSite.png'
 import hangloose from '../../images/hangloose.png'
 import acostamento from '../../images/acostamento.jpg'
-import labella from '../../images/labella.png'
-import tata from '../../images/tata.webp'
-import santander from '../../images/santander.png'
+import labella from '../../images/labellaSite.png'
+import tata from '../../images/tata-site.png'
+import santander from '../../images/santanderSite.png'
 
 const PROJETOS = [
     {
@@ -17,35 +16,40 @@ const PROJETOS = [
         image: acostamento,
         alt: "logo loja acostamento",
         title: "LOJA ACOSTAMENTO",
-        text: "Desenvolvimento de páginas de catálogo, página de produto entre outras para o e-commerce da seralle "
+        text: "Desenvolvimento de páginas de catálogo, página de produto entre outras para o e-commerce da seralle, ",
+        link: 'https://www.santander.com.br/hotsite/santanderfinanciamentos/'
     },
     {
         key: 3,
         image: labella,
         alt: "logo loja labella",
         title: "LOJA LABELLA LAMAFIA",
-        text: "Criação da página PlayBoy, melhorias na usabilidade da loja, programação"
+        text: "Durante uma das empresas que passei fiquei encarregado da crianção da página da parceria entre a loja Labella Mafia com a PlayBoy. A plataforma utilizada vtex io para codificação, html css e javascript.\n Álem da página fui responsavel pela estilização de unimeros componentes dentro do site Labella",
+        link: 'https://www.santander.com.br/hotsite/santanderfinanciamentos/'
     },
     {
         key: 6,
         image: santander,
         alt: "logo santander",
         title: "SANTANDER FINANCIAMENTO",
-        text: "Parto do time de desenvolvimento da aplicação web santander financiamento, programei no front-end da aplicação."
+        text: "Santander Financiamento foi um dos maiores projetos que participei, e também um dos que tenho maior orgulho, nele participei do time de front-end codificando a parte visual do site, ajustando erros e melhorando a usabilidades na navegação do cliente",
+        link: 'https://www.santander.com.br/hotsite/santanderfinanciamentos/'
     },
     {
         key: 4,
         image: tata,
         alt: "logo tatazentilbeauty",
-        title: "TATAZENTILbEAUTY",
-        text: "Criação do site institucional para divulgação de serviçõs no setor de beleza"
+        title: "TATAZENTILBEAUTY",
+        text: "Tenho muito orgulho deste projeto por ter participado e feito do zero, Ele foi criado para a lash designer Thais Zentil aumentar sua relevancia na web e divulgar seu trabalho, aumentando assim o número de leads para seu negócio. O projeto foi feito utilizando a linguagem de prorgamação React, tirando vantadem do seu sistema de componentização, no modelo one page podemos visualizar todo conteudo facilmente com o sistema de arcoragem, sendo levado aos conteúdos através do menu ou footer ",
+        link: 'https://tatazentilbeauty.com.br/'
     },
     {
         key: 1,
         image: seralle,
         alt: "logo loja seralle",
         title: "LOJA SERALLE",
-        text: "Prestação de serviço para melhorias da loja, Programação"
+        text: "Prestação de serviço para melhorias da loja, Programação",
+        link: 'https://www.seralle.com.br/'
     },
     {
         key: 2,
@@ -62,18 +66,14 @@ function PageProj(props) {
             <Header />
             <div className={styles.container}>
                 {PROJETOS.map(item => 
-                props.key == item.key &&
+                1 === item.key &&
                     <>
                         <h1 className={styles.containerImageList}>{item.title}</h1>
                         <div className={styles.containerProj}>
-                            <img className={styles.ImageUnique} src={item.src} alt={item.alt} />
+                            <a href={item.link} target='_blank'><img className={styles.ImageUnique} src={item.image} alt={item.alt} /></a>
                             <div>
-                                <p>
-                                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse posuere vel arcu cursus ornare. Morbi faucibus arcu vel nibh eleifend, eget tempus velit finibus. Vivamus       molestie eros eget dolor gravida, et vestibulum nisl condimentum. Nullam nec neque non ante semper varius. Vivamus velit sem, pharetra vitae orci a, elementum porta odio. Morbi a ipsum consectetur, porta dui at, tincidunt lorem. Vestibulum vitae turpis sollicitudin, lacinia libero quis, varius lectus. Sed ultrices erat eu risus venenatis tristique.
-
-                                In quis diam eu tortor accumsan hendrerit. Praesent lacus dui, sollicitudin sed mi eget, dictum ornare massa. Praesent ante ex, vehicula non leo a, accumsan convallis ipsum. Nulla lorem turpis, consectetur in mollis nec, iaculis gravida lacus. Donec facilisis justo enim, vel vulputate ex tincidunt quis. Maecenas egestas ut justo non euismod. Donec vel pulvinar lectus, ac efficitur lacus. Proin porta sem tempus ipsum commodo, non feugiat nulla bibendum. Nulla facilisi. Curabitur finibus ornare erat eu laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec nec augue ut nisi venenatis suscipit. Ut accumsan a lectus eget iaculis. Nullam mattis ullamcorper eros at fringilla. Mauris ac fermentum mi, eget sagittis augue. */}
-                                    {item.text}
-                                </p>
+                                <p>{item.text}</p>
+                                <a href={item.link} target='_blank'>Confira o Projeto</a>
                             </div>
                         </div>
                     </>
