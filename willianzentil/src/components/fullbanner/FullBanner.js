ed858bcad8f6575c5b9805971790a5d7a28d6ed2
willianZentil <willian.zentil@maeztra.com>
2023-12-28 14:21:05 -0300

    feature#banner-mobile

--- willianzentil/src/components/fullbanner/FullBanner.js
+++ willianzentil/src/components/fullbanner/FullBanner.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import fullbanner from '../../images/Home.png'
+import fullbannerMob from '../../images/Entusiasta pela área de visualização de dados e pelo mundo do ecommerce.png'
 import styles from './FullBanner.module.css'
 
 function FullBanner() {
   return (
     <section className={styles.ContainerfullBanner}>
+      <div className={styles.desktop}>
         <img className={styles.fullBanner} src={fullbanner} alt='Banner Home Willian Zentil' />
+      </div>
+      <div className={styles.mobile}>
+        <img className={styles.fullBanner} src={fullbannerMob} alt='Banner Home Willian Zentil Mobile' />
+      </div>
     </section>
   )
 }
